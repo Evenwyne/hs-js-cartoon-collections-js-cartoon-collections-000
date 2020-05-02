70a1313f2db2ebaa71eb2631b4a4422684ac9506
evenwyne <dubbwins@gmail.com>
2020-05-02 15:41:08 -0700

    Completed Cartoon Collections

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 function dwarfRollCall(dwarves) {
-    // Test
+    var result = ""
+    for (let i = 0; i < dwarves.length; i++) {
+        result += `${i + 1}. ` + dwarves[i] + ` `
+    }
+    return result
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+    var result = []
+    for (let i = 0; i < planeteerCalls.length; i++) {
+        result[i] = planeteerCalls[i].toUpperCase() + `!`
+        
+    }
+   return result
 }
 
 function longPlaneteerCalls(words) {
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > 4) {
+            return true
+        } else {
+            return false
+        }
+         
+    }
 }
 
 function findTheCheese (foods) {
+    var cheeses = ["cheddar", "gouda", "camembert"]
+    for(let i = 0; i < foods.length; i++) {
+        for(let j = 0; j < cheeses.length; j++) {
+            if(foods[i] == cheeses[j]) {
+                return cheeses[j]
+            }
+        }
+    }
+    return "no cheese!"
 }
+
