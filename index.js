fd162a7d62699685a5732234e2b84bccfcfdd68c
EVEN RICHARDSON <dubbwins@gmail.com>
2020-05-04 21:12:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 function dwarfRollCall(dwarves) {
-    var result = ""
-    for (let i = 0; i < dwarves.length; i++) {
-        result += `${i + 1}. ` + dwarves[i] + ` `
-    }
+  var result = ""
+  for (let i = 0; i < dwarves.length; i++) {
+    result += `${i + 1}. ` + dwarves[i] + ` `  }
     return result
 }
 
 function summonCaptainPlanet(planeteerCalls) {
-    var result = []
-    for (let i = 0; i < planeteerCalls.length; i++) {
-        result[i] = planeteerCalls[i].toUpperCase() + `!`
-        
-    }
+   var result = []
+   for (let i = 0; i < planeteerCalls.length; i++) {
+    result[i] = planeteerCalls[i].toUpperCase() + `!`
+   }
    return result
 }
 
+
 function longPlaneteerCalls(words) {
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].length > 4) {
-            return true
-        } else {
-            return false
-        }
-         
-    }
+   for (let i = 0; i < words.length; i++) {
+     if (words.length > 4)
+     return true
+   }
+   return false
 }
 
 function findTheCheese (foods) {
-    var cheeses = ["cheddar", "gouda", "camembert"]
-    for(let i = 0; i < foods.length; i++) {
-        for(let j = 0; j < cheeses.length; j++) {
-            if(foods[i] == cheeses[j]) {
-                return cheeses[j]
-            }
-        }
-    }
-    return "no cheese!"
+  var cheeses = ["cheddar", "gouda", "camembert"]
+  for (let i = 0; i < foods.length; i++) {
+  for (let j = 0; j < cheeses.length; j++) {
+    if (foods[i] == cheeses[j])
+    return cheeses[j]
+     }
+  }
+return "no cheese!"
 }
-
