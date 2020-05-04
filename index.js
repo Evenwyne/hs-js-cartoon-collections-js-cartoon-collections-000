281aebc825dd4b5303f309c7fcb854a55092231b
EVEN RICHARDSON <dubbwins@gmail.com>
2020-05-04 21:25:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,3 +32,9 @@ function findTheCheese (foods) {
   }
 return "no cheese!"
 }
+
+function wordsWithB (words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].startsWith("B"))
+  }
+}
